6f123df512eb8c9490e75e7c4792e30438261273
Batnaidan Tsend <batnaidan@gmail.com>
2021-05-23 23:38:38 +0800

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI:
-    'mongodb+srv://root:KQcCCcpzveDnQx3N@cluster0.3mmvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-  secretOrKey: 'chichadlaaldaa',
+  mongoURI: '',
+  secretOrKey: '',
 };
